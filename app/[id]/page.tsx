de9ee9a36c29f098b122511918e6215c37e71ca5
Yukine213 <kondratenkov1109@gmail.com>
2024-02-03 17:57:48 +0200

    fix the ui bug

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,5 +1,5 @@
+import React from "react";
 import { fetchWaifu } from "@/actions/waifu.actions";
-import { connectDb } from "@/lib/connectDb";
 import Image from "next/image";
 import { WaifuProps } from "../page";
 import { AiFillHeart } from "react-icons/ai";
@@ -8,26 +8,35 @@ const AboutWaifu = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
   const waifu: WaifuProps = await fetchWaifu(id);
   const hasAdded = 400;
+
   return (
-    <article className="px-3 mt-10  ">
-      <section className="bg-black-2  md:w-2/3 lg:w-3/4 mx-auto  ">
-        <div className="flex gap-4 justify-between">
-          <div className="grid grid-cols-2 gap-4">
-            <Image
-              src={waifu.image}
-              alt={waifu.name}
-              width={300}
-              height={300}
-            />
+    <article className="mt-10">
+      <section className="bg-black-2 md:w-2/3 lg:w-3/4 mx-auto flex gap-8 max-w-full">
+        <div className="">
+          <Image
+            src={waifu.image}
+            alt={waifu.name}
+            width={300}
+            height={300}
+            className="w-fit"
+          />
+        </div>
+        <div className="w-full flex flex-col ">
+          <div className="flex justify-between">
             <h4 className="text-xl pt-4">{waifu.name}</h4>
-            <div className="grid">{waifu.desc}</div>
+            <h4 className="text-xl pt-4">
+              Appears in: <span className="text-base">{waifu.appearsIn}</span>
+            </h4>
+            <div className="flex justify-between items-center ">
+              <div className="flex h-12 w-32 cursor-default items-center justify-center bg-pink-500 text-center text-lg text-white">
+                <AiFillHeart className="mr-2 h-6 w-6 fill-current" />
+                <span>{hasAdded}</span>
+              </div>
+            </div>
           </div>
-          <h4 className="pt-4 text-x">
-            Appears in: <span>{waifu.appearsIn}</span>
-          </h4>
-          <div className="flex h-12 w-32 cursor-default items-center justify-center bg-pink-500 text-center text-lg text-white">
-            <AiFillHeart className="mr-2 h-6 w-6 fill-current" />
-            <span>{hasAdded}</span>
+          <div className="mt-6">
+            <h4 className="text-xl">Description</h4>
+            <p className="text-gray-400 w-full">{waifu.desc}</p>
           </div>
         </div>
       </section>
