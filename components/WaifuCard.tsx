356b2ee34defcdf4f265e68dde43e53e059b837f
Yukine213 <kondratenkov1109@gmail.com>
2024-02-03 20:06:15 +0200

    fix the height difference between the cards

--- components/WaifuCard.tsx
+++ components/WaifuCard.tsx
@@ -5,15 +5,17 @@ import Link from "next/link";
 const WaifuCard = ({ waifu }: { waifu: WaifuProps }) => {
   return (
     <Link href={`${waifu._id}`}>
-      <div className="bg-black-2 w-[340px] rounded-md overflow-hidden ">
-        <Image
-          src={waifu.image}
-          alt={waifu.name}
-          width={300}
-          height={64}
-          className="w-full "
-        />
-        <div className="text-center space-y-2 ">
+      <div className="bg-black-2 w-[340px] rounded-md overflow-hidden pb-1">
+        <div className="w-full h-[300px]">
+          <Image
+            src={waifu.image}
+            alt={waifu.name}
+            width={350}
+            height={350}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="text-center space-y-2">
           <h3>{waifu.name}</h3>
           <hr className="opacity-20 w-9/12 mx-auto" />
           <h3>{waifu.appearsIn}</h3>
