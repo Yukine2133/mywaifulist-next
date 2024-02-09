7bd7881526a878288e5cc33ce54cbeac85f64033
Yukine213 <kondratenkov1109@gmail.com>
2024-02-09 19:47:56 +0200

    Implement conditional rendering of waifus or text message based on waifusArray length and user profile ownership

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -37,28 +37,44 @@ const Profile = async ({ params }: { params: { userId: string } }) => {
       ) : (
         <h6 className="mt-4 text-gray-400">
           The waifus that
-          <span className="text-gray-200"> {user.given_name}</span> have created
+          <span className="text-gray-200"> {user.given_name}</span> has created
         </h6>
       )}
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mx-auto gap-8 w-fit mt-10 mb-4">
-        {waifusArray.map((waifu) => (
-          <WaifuCard key={waifu._id} waifu={waifu} />
-        ))}
-      </div>
+      {waifusArray.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mx-auto gap-8 w-fit mt-10 mb-4">
+          {waifusArray.map((waifu) => (
+            <WaifuCard key={waifu._id} waifu={waifu} />
+          ))}
+        </div>
+      ) : (
+        <h6 className="mt-6 text-sm mb-4">
+          {profileBelongsToCurrentUser
+            ? "You haven't created any waifus yet"
+            : `${user.given_name} hasn't created any waifus yet`}
+        </h6>
+      )}
 
       {profileBelongsToCurrentUser ? (
         <h6 className="mt-4">The waifus that you have liked </h6>
       ) : (
         <h6 className="mt-4">
           The waifus that{" "}
-          <span className="text-gray-200">{user.given_name}</span> have liked
+          <span className="text-gray-200">{user.given_name}</span> has liked
         </h6>
       )}
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mx-auto gap-8 w-fit mt-10 mb-4">
-        {likedWaifus.map((likedWaifu) => (
-          <WaifuCard key={likedWaifu._id} waifu={likedWaifu} />
-        ))}
-      </div>
+      {likedWaifus.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 mx-auto gap-8 w-fit mt-10 mb-4">
+          {likedWaifus.map((likedWaifu) => (
+            <WaifuCard key={likedWaifu._id} waifu={likedWaifu} />
+          ))}
+        </div>
+      ) : (
+        <h6 className="mt-6 text-sm">
+          {profileBelongsToCurrentUser
+            ? "You haven't liked any waifus yet"
+            : `${user.given_name} hasn't liked any waifus yet`}
+        </h6>
+      )}
     </div>
   );
 };
